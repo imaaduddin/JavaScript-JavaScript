caf1e30cd2496c610b29678085ea26589055341f
Imaad Uddin <imaaduddin@icloud.com>
2021-07-27 21:05:02 -0500

    numbers

---
+++ numbers.js
@@ -0,0 +1,21 @@
+// Addition 
+console.log(1 + 8);
+
+// Subtraction 
+console.log(9 - 7);
+
+// Multiplication 
+console.log(23 * 2);
+
+// Division 
+console.log(45 / 5);
+
+// Modulo %
+console.log(25 % 5);
+console.log(25 % 3);
+
+// Exponential **
+console.log(8 ** 2);
+
+// PEMDAS
+// 1. () 2. ** 3. * 4. / 5. + 6. - (The order in which you do first)

ec80efdd1066c711c6e1b6bda099ee12deb4c659
Imaad Uddin <imaaduddin@icloud.com>
2021-08-04 14:53:23 -0500

    comparisons

--- comparisons.js
+++ comparisons.js
@@ -2,7 +2,18 @@
 < // less than
 >= // greater than or equal to
 <= // less than or equal to
+
+
 == // equality 
 != // not equal 
 === // strict equality 
 !== // strict non equality 
+
+// == vs ===
+// == (double equals) checks for equality of value, but not equality of type
+// It coerces both values to the same type and then compares them
+// This can lead to some unexpected results
+
+// === (triple equals) checks for equality of value AND type
+// ALWAYS GO WITH TRIPLE EQUALS
+// ALSO ALWAYS GO WITH !==

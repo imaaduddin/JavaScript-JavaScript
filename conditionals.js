a7e2b74d871dd9df8c37bca7a3b9754a26a49d31
Imaad Uddin <imaaduddin@icloud.com>
2021-08-09 13:49:02 -0500

    if else if else

---
+++ conditionals.js
@@ -0,0 +1,26 @@
+// IF - Run code IF a given condition is true
+
+if (1 === 1) {
+  console.log("It's True!");
+}
+
+// ELSE IF - If not the first thing, maybe this other thing??
+let rating = 2;
+if (rating === 3) {
+  console.log("YOU ARE A LEGEND!!!!");
+} 
+else if (rating === 2) {
+  console.log("YOU'RE ALRIGHT....");
+}
+
+// ELSE - If nothing else was true, do this...
+let score = 2;
+if (score === 3) {
+  console.log("YOU ARE A LEGEND!!!!");
+} 
+else if (score === 2) {
+  console.log("YOU'RE ALRIGHT....");
+}
+else {
+  console.log("YOU EITHER ARE REALLY GOOD OR REALLY BAD...");
+}

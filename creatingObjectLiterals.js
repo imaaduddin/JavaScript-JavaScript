682adead2df25c3d15d6af15b0eed56f840ab3d7
Imaad Uddin <imaaduddin@icloud.com>
2021-08-20 15:40:45 -0500

    objects

---
+++ creatingObjectLiterals.js
@@ -0,0 +1,10 @@
+const fitBitData = {
+  totalSteps: 30289,
+  totalMiles: 211.7,
+  avgCalorieBurn: 5755,
+  workoutsThisWeek: "5 of 7",
+  avgGoodSleep: "2:13"
+};
+
+// To access data within object:
+fitBitData.totalMiles; // will print out 211.1
---
+++ introToObjects.js
@@ -0,0 +1,12 @@
+// OBJECTS 
+// Objects are collections of properties 
+// Properties are a key value pair 
+// Rather than accessing data using an index, we use custom keys 
+
+const fitBitData = {
+  totalSteps: 30289,
+  totalMiles: 211.7,
+  avgCalorieBurn: 5755,
+  workoutsThisWeek: "5 of 7",
+  avgGoodSleep: "2:13"
+}

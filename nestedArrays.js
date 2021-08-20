5b443865b9ee3d158f24eabb1603c9789bee8847
Imaad Uddin <imaaduddin@icloud.com>
2021-08-20 15:27:29 -0500

    arrays and array methods

---
+++ nestedArrays.js
@@ -0,0 +1,7 @@
+// NESTED ARRAYS - We can store arrays inside other arrays!
+const colors = [
+  ["red", "orange"],
+  ["yellow", "blue"],
+  ["black", "gold"]
+];
+console.log(colors[0]);
--- referenceTypes.js
+++ referenceTypes.js
@@ -1,1 +1,10 @@
 // Value Type Variable 
+let fruit = "orange";
+let color = fruit;
+// fruit stores "orange" and color stores "orange as well
+
+
+const ninja = ["Narutp", "Kiba"];
+// You can do anything you want to this array EXCEPT REASSIGNING IT to something else for example:
+ninja = ["Choji", "Neji"];
+console.log(ninja);

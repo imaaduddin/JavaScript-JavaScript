fb65ea5aea796c072befc2631ad2aee2eeebb6b8
Imaad Uddin <imaaduddin@icloud.com>
2021-08-31 15:15:25 -0500

    loops

--- arrayAndObjectEquality.js
+++ arrayAndObjectEquality.js
@@ -1,7 +1,19 @@
 let nums = [1, 2, 3];
 let mystery = [1, 2, 3];
 
-nums === mystery; // will five you false 
+nums === mystery; // will five you false // they look the same, but refer to different arrays 
 
 let moreNums = nums;
-moreNums === mystery; // will give you true because they are referring to same place in memory 
+moreNums === mystery; // will give you true because they are referring to same place in memory // these two arrays reference the exact same array, so we get true
+
+const user = {
+  username: "Sharingan13",
+  email: "obitouchiha@gmail.com",
+  notifications: []
+};
+
+if (!user.notifications.length) {
+  console.log("No new notifications!");
+}
+
+
---
+++ loops.js
@@ -0,0 +1,9 @@
+// LOOPS
+// Loops allows us to repeat code 
+// Print "hello" 10 times
+// Sum all numbers in an array 
+// There are multiple types of loops:
+// for loop
+// while loop
+// for...of loop
+// for...in loop

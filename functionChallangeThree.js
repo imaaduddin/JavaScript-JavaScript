77f4f3d1bf44427b4c9da4672d32b4e3f779ef67
Imaad Uddin <imaaduddin@icloud.com>
2021-09-28 23:30:04 -0500

    function challange

---
+++ functionChallangeThree.js
@@ -0,0 +1,10 @@
+// A pangram is a sentence that contains every letter of the alphabet 
+// Write a function called isPangram, which checks to see if a given sentence contains every letter of the alphabet. make sure ot ignore string casing 
+
+let aString = "abcdefghijklmnopqrstuvwxyz";
+
+let test = "hi my name is naruto, i love ramen"
+
+function isPangram(str) {
+  
+}

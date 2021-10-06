2ef2d4d8f2d94366d16c61cb0b80208cb56d418d
Imaad Uddin <imaaduddin@icloud.com>
2021-10-06 00:16:54 -0500

    pangram challange

--- functionChallangeThree.js
+++ functionChallangeThree.js
@@ -1,10 +1,24 @@
 // A pangram is a sentence that contains every letter of the alphabet 
 // Write a function called isPangram, which checks to see if a given sentence contains every letter of the alphabet. make sure ot ignore string casing 
 
-let aString = "abcdefghijklmnopqrstuvwxyz";
-
-let test = "hi my name is naruto, i love ramen"
+// function isPangram(str) {
+//   let lowercase = str.toLowerCase();
+//   for (let char of "abcdefghijklmnopqrstuvwxyz") {
+//     if (lowercase.indexOf(char) === -1) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
 function isPangram(str) {
-  
+  let lowercase = str.toLowerCase();
+  for (let char of "abcdefghijklmnopqrstuvwxyz") {
+    if (!lowercase.includes(char)) {
+      return false;
+    }
+  }
+  return true;
 }
+
+console.log(isPangram("The five boxing wizards jump quicly"));
